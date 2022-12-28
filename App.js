6dad11f7680e7a7950cdfc9c17124ee695f44702
JRM <johnrhmartin@gmail.com>
2022-12-28 13:24:57 -0500

    moved WebView to export function App()

--- App.js
+++ App.js
@@ -5,14 +5,59 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 import { WebView } from 'react-native-webview';
 
-<WebView
-  source={{
-    uri: 'https://hextorgbapro.com/index.html',
-  }}
-  style={{ height: '100%' }}
-  javaScriptEnabled={true}
-  domStorageEnabled={true}
-  injectedJavaScript={`
+
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const {
+  width: SCREEN_WIDTH,
+  height: SCREEN_HEIGHT,
+} = Dimensions.get('window');
+
+const scaleWidth = SCREEN_WIDTH / 360;
+const scaleHeight = SCREEN_HEIGHT / 800;
+
+const WidthRatio = (size) => {
+  const newSize = size * scaleWidth;
+  return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+}
+
+const HeightRatio = (size) => {
+  const newSize = size * scaleHeight;
+  return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+}
+
+export default function App() {
+  return (
+    <>
+      <LinearGradient
+        colors={['#181818', '#000000']}
+        style={{ opacity: 1, width: windowWidth, alignSelf: 'center', flex: 1 }}
+      // start={[0.0, 0.5]} end={[1.0, 0.5]}
+      >
+        <View >
+
+          <View style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)', height: '4rem' }}>
+            <Text style={{ color: '#b892ff', alignSelf: 'center', marginTop: '0.8rem', fontSize: '2rem', fontWeight: 'bold', fontFamily: 'Inter_900Black', }}>HEX to RGBA Pro</Text>
+          </View>
+
+          <View style={styles.container}>
+            <Convert />
+            <StatusBar style="auto" />
+          </View>
+
+        </View>
+      </LinearGradient>
+      <WebView
+        source={{
+          uri: 'https://hextorgbapro.com/index.html',
+        }}
+        style={{ height: '100%' }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        injectedJavaScript={`
     (function() {
       // Add your AdSense code here
       var script = document.createElement('script');
@@ -30,50 +75,8 @@ import { WebView } from 'react-native-webview';
       (adsbygoogle = window.adsbygoogle || []).push({});
     })();
   `}
-/>
-
-
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
-const {
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
-} = Dimensions.get('window');
-
-const scaleWidth = SCREEN_WIDTH / 360;
-const scaleHeight = SCREEN_HEIGHT / 800;
-
-const WidthRatio = (size) => {
-    const newSize = size * scaleWidth;
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
-}
-
-const HeightRatio = (size) => {
-    const newSize = size * scaleHeight;
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
-}
-
-export default function App() {
-  return (
-    <LinearGradient
-        colors={['#181818', '#000000' ]}
-        style={{ opacity: 1, width: windowWidth, alignSelf: 'center', flex: 1 }}
-        // start={[0.0, 0.5]} end={[1.0, 0.5]}
-      >
-    <View >
-
-    <View style ={{backgroundColor: 'rgba(255, 255, 255, 0.05)', height: '4rem'}}>
-      <Text style={{color: '#b892ff', alignSelf: 'center', marginTop: '0.8rem', fontSize: '2rem', fontWeight: 'bold', fontFamily: 'Inter_900Black',}}>HEX to RGBA Pro</Text>
-    </View>
-
-      <View style={styles.container}>
-        <Convert />
-        <StatusBar style="auto" />
-      </View>
-      
-    </View>
-    </LinearGradient>
+      />
+    </>
   );
 }
 
