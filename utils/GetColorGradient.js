3994178669def3546d15d6197ae171a781afcd71
JRM <johnrhmartin@gmail.com>
2022-12-28 01:41:19 -0500

    Updates

--- utils/GetColorGradient.js
+++
@@ -1,6 +0,0 @@
-import chroma from 'chroma-js';
-
-export const GetColorGradient = (color) => {
-  const scale = chroma.scale([chroma(color).brighten(2), color, chroma(color).darken(2)]);
-  return scale.colors(10);
-};
