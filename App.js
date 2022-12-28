b4cb9707ebde82f42e037d8e04af458597c2489e
JRM <johnrhmartin@gmail.com>
2022-12-28 13:12:46 -0500

    webview adsense code

--- App.js
+++ App.js
@@ -3,6 +3,35 @@ import { StyleSheet, Text, View, Dimensions, PixelRatio, Image } from 'react-nat
 import { Convert } from './Convert'
 import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { WebView } from 'react-native-webview';
+
+<WebView
+  source={{
+    uri: 'https://hextorgbapro.com/index.html',
+  }}
+  style={{ height: '100%' }}
+  javaScriptEnabled={true}
+  domStorageEnabled={true}
+  injectedJavaScript={`
+    (function() {
+      // Add your AdSense code here
+      var script = document.createElement('script');
+      script.async = true;
+      script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+      var ins = document.createElement('ins');
+      ins.className = 'adsbygoogle';
+      ins.style.display = 'block';
+      ins.setAttribute('data-ad-client', 'ca-pub-3255964932603196');
+      ins.setAttribute('data-ad-slot', '');
+      ins.setAttribute('data-ad-format', 'auto');
+      ins.setAttribute('data-full-width-responsive', 'true');
+      document.body.appendChild(script);
+      document.body.appendChild(ins);
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    })();
+  `}
+/>
+
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -18,11 +47,6 @@ const scaleHeight = SCREEN_HEIGHT / 800;
 const WidthRatio = (size) => {
     const newSize = size * scaleWidth;
     return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
-    //   if (Platform.OS === 'ios') {
-    //     return Math.round(PixelRatio.roundToNearestPixel(newSize))
-    //   } else {
-    //     return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
-    //   }
 }
 
 const HeightRatio = (size) => {
@@ -30,43 +54,19 @@ const HeightRatio = (size) => {
     return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
 }
 
-const DisplayGradient = () => {
-  return (
-    <>
-      <LinearGradient
-        // colors={['#5390d9', '#4361ee']}
-        colors={['#33415c', '#181818']}
-        style={{ opacity: 1, height: '7rem', width: '95vw', marginTop: '1rem', alignSelf: 'center', borderRadius: '10rem', flex: 1 }}
-        // start={[0.0, 0.5]} end={[1.0, 0.5]}
-      >
-        <Text style={{color: 'white', alignSelf: 'center', marginTop: '0.8rem', fontSize: '2.9rem', fontWeight: 'bold'}}>Hex to RGBA Pro</Text>
-        <View style={{borderBottomWidth: 6, borderBottomColor: '#ff006e', width: '20rem', alignSelf: 'center'}}></View>
-      </LinearGradient>
-    </>
-  )
-}
-
 export default function App() {
   return (
     <LinearGradient
-        // colors={['#5390d9', '#4361ee']}
         colors={['#181818', '#000000' ]}
         style={{ opacity: 1, width: windowWidth, alignSelf: 'center', flex: 1 }}
         // start={[0.0, 0.5]} end={[1.0, 0.5]}
       >
     <View >
-      {/* <Image source={require('./assets/favicon.png')} style={{width: '10rem', height: '10rem', position: 'absolute', height: '4rem', width: '4rem', top: '0.5rem', left: '1rem'}} />
-    <View style={{borderBottomWidth: 2, borderBottomColor: 'rgba(255, 255, 255, 0.2)', width: windowWidth, marginTop: '5rem', alignSelf: 'center'}}></View>
-    <View style={{borderLeftWidth: 2, borderLeftColor: 'rgba(255, 255, 255, 0.2)', height: windowHeight, marginLeft: '6rem', position: 'absolute'}}></View> */}
+
     <View style ={{backgroundColor: 'rgba(255, 255, 255, 0.05)', height: '4rem'}}>
       <Text style={{color: '#b892ff', alignSelf: 'center', marginTop: '0.8rem', fontSize: '2rem', fontWeight: 'bold', fontFamily: 'Inter_900Black',}}>HEX to RGBA Pro</Text>
     </View>
-    
-      
-    {/* <DisplayGradient /> */}
-    {/* <View style={{backgroundColor: '#f5cb5c', height: '5rem', width: windowWidth, borderBottomRightRadius: '2rem', borderBottomLeftRadius: '2rem'}}>
-        <Text style={{color: '#000814', alignSelf: 'center', marginTop: '0.8rem', fontSize: '2.9rem', fontWeight: 'bold'}}>HEX To RGBA Pro</Text>
-    </View> */}
+
       <View style={styles.container}>
         <Convert />
         <StatusBar style="auto" />
